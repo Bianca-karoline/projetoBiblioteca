5d15d4646dc5a5e259dc8a66bb9e7982aba2e71d
Leonardo Emanuel <116128056+leoemfs@users.noreply.github.com>
2023-05-28 03:05:48 -0300

    Update Biblioteca.java

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Biblioteca {
 	ArrayList <ItemBiblioteca> acervo;
-	//verificar de colocar num map para poder ordenar os itens em ordem alfabetica
+	//usar metodo sort() para ordenar por ordem alfabetica por titulo
 	
 	//metodo para adicionar item ao array acervo
 	
