bb22e3fd1ff50a805abaf374908506859bfab54f
Leonardo Emanuel <116128056+leoemfs@users.noreply.github.com>
2023-05-28 20:17:00 -0300

    Update Revista.java

--- src/Revista.java
+++ src/Revista.java
@@ -1,4 +1,19 @@
 
-public class Revista extends MaterialBiblioteca{
+public class Revista extends ItemBiblioteca{
+	private int numEdicao;
 
+	public Revista(String titulo, String autor, String editora, String categoria, int qtdExemplares, int numEdicao) {
+		super(titulo, autor, editora, categoria, qtdExemplares);
+		this.numEdicao = numEdicao;
+	}
+	
+	public boolean verificarDisponibilidade() {
+		boolean disponibilidade;
+		if(getQtdExemplares() <= 0) {
+			disponibilidade = false;
+		}else {
+			disponibilidade = true;
+		}
+		return disponibilidade;
+	}
 }
