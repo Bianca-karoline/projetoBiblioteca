eee9884255ad2db3f0b028745d31ea7914614979
Leonardo Emanuel <116128056+leoemfs@users.noreply.github.com>
2023-05-28 03:13:47 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,4 +1,17 @@
 
-public class Livro extends MaterialBiblioteca{
+public class Livro extends ItemBiblioteca{
+	private int anoPublicacao;
+	
+	public Livro(String titulo, String autor, String editora, String categoria, int qtdExemplares, int anoPublicacao) {
+		super(titulo, autor, editora, categoria, qtdExemplares);
+		this.anoPublicacao = anoPublicacao;
+	}
 
+	public void verificarDisponibilidade() {
+		if(getQtdExemplares() <= 0) {
+			System.out.println("Indisponivel. Item esgostado!");
+		}else {
+			System.out.println("Disponivel. Quantidades de exemplares: " + getQtdExemplares());
+		}
+	}
 }
