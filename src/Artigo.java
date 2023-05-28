6a84883234df395b6f35515935b80e4dd42458bd
Leonardo Emanuel <116128056+leoemfs@users.noreply.github.com>
2023-05-28 20:08:23 -0300

    Update Artigo.java

--- src/Artigo.java
+++ src/Artigo.java
@@ -1,4 +1,32 @@
 
-public class Artigo extends MaterialBiblioteca{
+public class Artigo extends ItemBiblioteca{
+	private String dataPublicacao;
 
+	public Artigo(String titulo, String autor, String editora, String categoria, int qtdExemplares,
+			String dataPublicacao) {
+		super(titulo, autor, editora, categoria, qtdExemplares);
+		this.dataPublicacao = dataPublicacao;
+	}
+	
+	public boolean verificarDisponibilidade() {
+		boolean disponibilidade;
+		if(getQtdExemplares() <= 0) {
+			disponibilidade = false;
+		}else {
+			disponibilidade = true;
+		}
+		return disponibilidade;
+	}
+  
+	public String toString() {
+		return "Data de publicação do Artigo: " + dataPublicacao;
+	}
+
+	public String getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(String dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
 }
