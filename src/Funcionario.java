b766a57b07fc910aa2102c69a895f20afb011a5a
Leonardo Emanuel Feitosa <116128056+leoemfs@users.noreply.github.com>
2023-06-13 23:13:54 -0300

    Delete src directory

--- src/Funcionario.java
+++
@@ -1,9 +0,0 @@
-
-public class Funcionario extends Usuario{
-	private String funcao;
-
-	public Funcionario(String nome, String cpf, String telefone, String email, String funcao) {
-		super(nome, cpf, telefone, email);
-		this.funcao = funcao;
-	}
-}
